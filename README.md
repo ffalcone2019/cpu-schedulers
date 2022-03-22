6762c69f8f147774872509df74a85a61a85960c1
John Falcone <John_Falcone@ncci.com>
2022-03-21 20:46:14 -0400

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cpu-schedulers
+
+NodeJS version of the CPU scheduler.  Test using:
+
+[https://www.jdoodle.com/execute-nodejs-online/](https://www.jdoodle.com/execute-nodejs-online/)
---
+++ main.js
@@ -0,0 +1,23 @@
+// Set up arrays for the 9 processes and print out the values of each array
+
+// The 9 process arrays
+var p1 = [ 7, 22, 6, 19, 12, 44, 8, 21, 10, 37, 5, 24, 6, 44, 7, 43, 8 ];
+var p2 = [ 14, 48, 15, 44, 17, 42, 22, 37, 19, 76, 14, 41, 16, 31, 17, 43, 18 ];
+var p3 = [ 8, 43, 7, 41, 6, 45, 8, 21, 9, 35, 14, 18, 5, 26, 3, 31, 6 ];
+var p4 = [ 13, 37, 4, 41, 5, 35, 12, 41, 8, 55, 15, 34, 6, 73, 5, 77, 3 ];
+var p5 = [ 6, 34, 7, 21, 5, 44, 6, 32, 7, 28, 3, 48, 11, 44, 6, 33, 3, 28, 4 ];
+var p6 = [ 9, 32, 4, 28, 5, 10, 6, 12, 7, 14, 9, 18, 12, 24, 15, 30, 8 ];
+var p7 = [ 14, 46, 17, 41, 11, 42, 15, 21, 4, 32, 7, 19, 16, 33, 10 ];
+var p8 = [ 4, 64, 5, 53, 6, 44, 4, 73, 6, 87, 5, 66, 8, 25, 6, 33, 9, 41, 7 ];
+var p9 = [ 13, 37, 8, 41, 7, 27, 12, 29, 5, 27, 6, 18, 3, 33, 4, 62, 6 ];
+
+// Print out the process arrays
+console.log(p1.toString())
+console.log(p2.toString())
+console.log(p3.toString())
+console.log(p4.toString())
+console.log(p5.toString())
+console.log(p6.toString())
+console.log(p7.toString())
+console.log(p8.toString())
+console.log(p9.toString())
